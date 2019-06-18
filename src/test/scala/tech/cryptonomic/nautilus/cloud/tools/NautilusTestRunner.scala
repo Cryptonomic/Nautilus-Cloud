584df93ffed01580d3050f60c00552b3ad0f8dca
Dorian Sarnowski <dorian.sarnowski@gmail.com>
2019-06-18 17:45:11 +0100

    22 - shut down nautilus cloud after each test suite

--- src/test/scala/tech/cryptonomic/nautilus/cloud/tools/NautilusTestRunner.scala
+++ src/test/scala/tech/cryptonomic/nautilus/cloud/tools/NautilusTestRunner.scala
@@ -3,19 +3,35 @@ package tech.cryptonomic.nautilus.cloud.tools
 import java.net.Socket
 
 import com.typesafe.scalalogging.StrictLogging
+import org.scalatest.{BeforeAndAfterEach, TestSuite}
 import tech.cryptonomic.nautilus.cloud.{NautilusCloud, NautilusContext}
 
 import scala.util.Try
 
-trait NautilusTestRunner extends StrictLogging {
+trait NautilusTestRunner extends BeforeAndAfterEach with StrictLogging {
+  self: TestSuite =>
 
   private val httpConfig = NautilusContext.httpConfig
+
+  val thread: Thread = new Thread {
+    override def run() {
+      logger.info("Starting Nautilus Cloud")
+      NautilusCloud.main(Array.empty)
+    }
+  }
+  thread.start()
+
+  // wait for a Nautilus to start
+  while (!isNautilusRunning) {
+    logger.info("Waiting for a Nautilus Cloud to start")
+    Thread.sleep(500)
+  }
 
   private def isNautilusRunning: Boolean =
     Try(new Socket(httpConfig.host, httpConfig.port).close()).isSuccess
 
-  if (isNautilusRunning)
-    logger.info("Reusing already running Nautilus Cloud on {} at port {}", httpConfig.host, httpConfig.port)
-  else
-    NautilusCloud.main(Array.empty)
+  override protected def afterEach(): Unit = {
+    thread.stop()
+    logger.info("Nautilus Cloud stopped successfully")
+  }
 }
